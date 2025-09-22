2b527e157e7095f30f3ccd9934dfdfd9d7560c68
Home Calculator User <user@example.com>
2025-09-22 13:30:13 -0700

    Add Firebase config for cloud sync

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
-// Paste your Firebase config object here. Example:
-// window.firebaseConfig = {
-//   apiKey: "...",
-//   authDomain: "...",
-//   projectId: "...",
-//   appId: "..."
-// };
-// Leave the variable name as window.firebaseConfig
-
-window.firebaseConfig = window.firebaseConfig || null;
+// Firebase configuration for Home Calculator
+window.firebaseConfig = {
+  apiKey: "AIzaSyBwMusPcd38oXIpeT6u8Wdj7n6M9MxJey4",
+  authDomain: "home-calculator-1f33a.firebaseapp.com",
+  projectId: "home-calculator-1f33a",
+  storageBucket: "home-calculator-1f33a.firebasestorage.app",
+  messagingSenderId: "838345925732",
+  appId: "1:838345925732:web:e5df5592c85f402b36eb71"
+};
 
